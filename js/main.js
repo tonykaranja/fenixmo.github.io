751e17e6587a52a88c8c761372ab6e2b6b5e4d3c
Tony Karanja <fenixpiconi@gmail.com>
2017-08-29 13:40:49 -0700

    added images and div on index

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ function myFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName('mySlides');
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length };
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+//
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex ++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000);
+}
